61e9066be1a91ae3bd9c7a90f78b7bca725c9b02
Guilherme Martins <guinm3@gmail.com>
2019-09-08 00:01:28 +0100

    novo template com algumas alterações e algumas imagens

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("21 October 2019 9:00:00 GMT+03:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
@@ -312,10 +312,10 @@ function makeTimer() {
 		if (minutes < "10") { minutes = "0" + minutes; }
 		if (seconds < "10") { seconds = "0" + seconds; }
 
-		$("#days").html(days + "<span>Days</span>");
-		$("#hours").html(hours + "<span>Hours</span>");
-		$("#minutes").html(minutes + "<span>Minutes</span>");
-		$("#seconds").html(seconds + "<span>Seconds</span>");		
+		$("#days").html(days + "<span>Dias</span>");
+		$("#hours").html(hours + "<span>Horas</span>");
+		$("#minutes").html(minutes + "<span>Minutos</span>");
+		$("#seconds").html(seconds + "<span>Segundos</span>");		
 
 }
 
