1362ef485b1d8a51e1be7fab8da879f247332ca4
Guilherme Martins <guinm3@gmail.com>
2019-09-22 16:40:25 -0300

    removed parallax and changed slider-text font-weight

--- js/main.js
+++ js/main.js
@@ -29,14 +29,14 @@
 	};
 
 
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
+	// $(window).stellar({
+ //    responsive: true,
+ //    parallaxBackgrounds: true,
+ //    parallaxElements: true,
+ //    horizontalScrolling: false,
+ //    hideDistantElements: false,
+ //    scrollProperty: 'scroll'
+ //  });
 
 
 	var fullHeight = function() {
